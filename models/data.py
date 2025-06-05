87d40d9725b30d999743eac137bd14760ea21551
zur4w <zurawskikamil42@gmail.com>
2025-06-05 17:54:36 +0200

    chore: add initial data

---
+++ models/data.py
@@ -0,0 +1,33 @@
+airports: list[dict] = [
+
+    {"name": "Lotnisko Chopina w Warszawie", "location": "Warszawa", "latitude": 52.165, "longitude": 20.967 },
+    {"name": "Port lotniczy Kraków Balice", "location": "Kraków", "latitude": 50.078, "longitude": 19.784 },
+    {"name": "Port Lotniczy Gdańsk im. Lecha Wałęsy", "location": "Gdańsk", "latitude": 54.378, "longitude": 18.466 },
+    {"name": "Port lotniczy Katowice-Pyrzowice", "location": "Katowice", "latitude": 50.474, "longitude": 19.080 },
+    {"name": "Port lotniczy Poznań-Ławica", "location": "Poznań", "latitude": 52.421, "longitude": 16.826 },
+
+]
+
+customers: list[dict] = [
+
+    {"name": "Jan", "surname": "Nowak", "location": "Kraków", "latitude": 50.078, "longitude": 19.784, "my_airport": "Port lotniczy Kraków Balice"},
+    {"name": "Will", "surname": "Smith", "location": "Warszawa", "latitude": 52.165, "longitude": 20.967, "my_airport": "Lotnisko Chopina w Warszawie"},
+    {"name": "Marek", "surname": "Niewierny", "location": "Warszawa", "latitude": 52.165, "longitude": 20.967, "my_airport": "Lotnisko Chopina w Warszawie"},
+    {"name": "Marzena", "surname": "Pomocna", "location": "Poznań", "latitude": 52.421, "longitude": 16.826, "my_airport": "Port lotniczy Poznań-Ławica"},
+    {"name": "Jorge", "surname": "Rodriguez", "location": "Poznań", "latitude": 52.421, "longitude": 16.826, "my_airport": "Port lotniczy Poznań-Ławica"},
+
+]
+
+employees: list[dict] = [
+
+    {"name": "Agata", "surname": "Szczera", "location": "Kraków", "latitude": 50.078, "longitude": 19.784, "workplace": "Port lotniczy Kraków Balice"},
+    {"name": "Frank", "surname": "Courtney", "location": "Kraków", "latitude": 50.078, "longitude": 19.784, "workplace": "Port lotniczy Kraków Balice"},
+    {"name": "Ewa", "surname": "Sucha", "location": "Warszawa", "latitude": 52.165, "longitude": 20.967, "workplace": "Lotnisko Chopina w Warszawie"},
+    {"name": "Michael", "surname": "Bednarek", "location": "Warszawa", "latitude": 52.165, "longitude": 20.967, "workplace": "Lotnisko Chopina w Warszawie"},
+    {"name": "Adam", "surname": "Smith", "location": "Poznań", "latitude": 52.421, "longitude": 16.826, "workplace": "Port lotniczy Poznań-Ławica"},
+    {"name": "Iwona", "surname": "Mikulska", "location": "Poznań", "latitude": 52.421, "longitude": 16.826, "workplace": "Port lotniczy Poznań-Ławica"},
+
+]
+
+
+
